b6b3dcf950817a66f87846206bba80ac71f3aa60
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-04-21 16:19:00 +0200

    GitHubDataProvider has been divided into several separate functions, each responsible for a smaller task.

--- src/main/java/pl/bartdel/allegrosummerexperience/service/GitHubDataProvider.java
+++ src/main/java/pl/bartdel/allegrosummerexperience/service/GitHubDataProvider.java
@@ -4,19 +4,43 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class GitHubDataProvider {
 
-
-    public String getData(String user) throws UnirestException {
+    private HttpResponse<JsonNode> getJSON(String user) throws UnirestException {
         HttpResponse<JsonNode> jsonResponse
                 = Unirest.get("https://api.github.com/users/" + user + "/repos")
                 .header("accept", "application/json")
                 .asJson();
-        return jsonResponse.getBody().getArray().toString();
+        return jsonResponse;
+    }
 
+    private int countStars(HttpResponse<JsonNode> repos){
+        return 0;
+    }
+
+    private ArrayList<String> getReposList(HttpResponse<JsonNode> jsonNodeHttpResponse){
+        JSONArray array = jsonNodeHttpResponse.getBody().getArray();
+        ArrayList<String> reposList = new ArrayList<>();
+        for(int i = 0; i < array.length(); i++){
+            reposList.add(array.getJSONObject(i).getString("name"));
         }
+        return reposList;
+    }
+
+    private String formatDataToJSON(int stars, ArrayList reposList){
+        return "placeholder";
+    }
+
+    public String getData(String user) throws UnirestException {
+        HttpResponse<JsonNode> response = getJSON(user);
+        return formatDataToJSON(countStars(response), getReposList(response));
 
     }
+
+}
