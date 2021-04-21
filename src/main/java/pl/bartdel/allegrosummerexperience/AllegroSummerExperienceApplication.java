0bc3a9aa32243890f6c801b90b572874f9627ac9
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-04-21 12:59:54 +0200

    Spring boot project has been initialized

---
+++ src/main/java/pl/bartdel/allegrosummerexperience/AllegroSummerExperienceApplication.java
@@ -0,0 +1,13 @@
+package pl.bartdel.allegrosummerexperience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroSummerExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroSummerExperienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bartdel/allegrosummerexperience/AllegroSummerExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartdel.allegrosummerexperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroSummerExperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
