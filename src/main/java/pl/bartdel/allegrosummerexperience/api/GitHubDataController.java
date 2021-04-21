55bd76c1d203170dd779cd2f7b56078fe127fba8
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-04-21 15:55:28 +0200

    Github API connector added

--- src/main/java/pl/bartdel/allegrosummerexperience/api/GitHubDataController.java
+++ src/main/java/pl/bartdel/allegrosummerexperience/api/GitHubDataController.java
@@ -1,4 +1,20 @@
 package pl.bartdel.allegrosummerexperience.api;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.bartdel.allegrosummerexperience.service.GitHubDataProvider;
+
+@RestController
+@RequestMapping("/api/")
 public class GitHubDataController {
+
+    GitHubDataProvider gitHubDataProvider = new GitHubDataProvider();
+
+    @GetMapping(value="/githubdata")
+    @ResponseBody
+    public String getUserData(@RequestParam(value = "user") String user) throws UnirestException {
+        return gitHubDataProvider.getData(user);
+    }
 }
--- src/main/java/pl/bartdel/allegrosummerexperience/service/GitHubDataProvider.java
+++ src/main/java/pl/bartdel/allegrosummerexperience/service/GitHubDataProvider.java
@@ -1,4 +1,22 @@
 package pl.bartdel.allegrosummerexperience.service;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GitHubDataProvider {
-}
+
+
+    public String getData(String user) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse
+                = Unirest.get("https://api.github.com/users/" + user + "/repos")
+                .header("accept", "application/json")
+                .asJson();
+        return jsonResponse.getBody().getArray().toString();
+
+        }
+
+    }
